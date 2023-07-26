21c47356ec30d3a9caa436d0087e5322eee6a207
ra11 <rajeswarivinayagam05@gmail.com>
2023-07-26 13:06:25 +0400

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+ 
+
+"""for num in range(5, 11 ):
+     print(num)"""
+
+"""number = 3
+
+print("The multiples are: ")
+for i in range(1,11):
+    print(number*i)"""
+    
+for i in range(1,10,2):
+     print(i)
+     git remote add origin https://github.com/ra11/Phyton.git
+git branch -M main
+git push -u origin main
