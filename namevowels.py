501927c48e937e8c56edfea90bd5f1a6e72a34a8
ra11 <rajeswarivinayagam05@gmail.com>
2023-07-31 14:07:51 +0400

    Add files via upload

--- namevowels.py
+++ namevowels.py
@@ -4,12 +4,24 @@ Created on Sun Jul 30 19:48:03 2023
 
 @author: sathy
 """
-
-input_string = input("Enter names: ")
-name_list = input_string.split(" ")
-for name in name_list:
- vowels = ('a', 'e', 'i', 'o', 'u');
- if any(char in vowels for char in name):
-    print (name, "is a vowel.")
- else:
-        print (name, "is not a vowel.")
+"""a=[]
+for name in range(5):
+ input_string = input("Enter names: ")
+ a.append(name)
+ vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}
+if any(char in vowels for char in  a):
+       print (a, "is a vowel.")
+else:
+        print (a, "is not a vowel.")"""
+        
+a=[] 
+vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}   
+for name in range(5):
+    inputname=input("Enter name: ")
+    a.append(inputname)
+    
+for name in a:
+     if any(char in vowels for char in name):
+        print (name, "is a vowel.")
+     else:
+         print (name, "is not a vowel.")
