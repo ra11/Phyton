54abb975ff55f75de2f9d8223d39f7b2f43ba56c
ra11 <rajeswarivinayagam05@gmail.com>
2023-07-31 14:53:53 +0400

    Add files via upload

--- namevowels.py
+++ namevowels.py
@@ -14,14 +14,14 @@ if any(char in vowels for char in  a):
 else:
         print (a, "is not a vowel.")"""
         
+""" print and append only vowels"""
 a=[] 
 vowels = {"a", "e", "i", "o", "u", "A", "E", "I", "O", "U"}   
 for name in range(5):
     inputname=input("Enter name: ")
-    a.append(inputname)
+    if any(char in vowels for char in inputname):
+     a.append(inputname)
     
 for name in a:
      if any(char in vowels for char in name):
         print (name, "is a vowel.")
-     else:
-         print (name, "is not a vowel.")
