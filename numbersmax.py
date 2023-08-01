d23e04dc926823732af8469875a47a95f2ae6310
ra11 <rajeswarivinayagam05@gmail.com>
2023-08-01 12:37:45 +0400

    Add files via upload

---
+++ numbersmax.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  1 12:17:54 2023
+
+@author: sathy
+"""
+
+"""a=[ 1, 3, 5, 7, 9]
+sm=sum(a)
+print("The sum of :", a) 
+print(sm)"""
+
+a=[ 1, 3, 5, 7, 9]
+sm=max(a)
+print("The max of :", a) 
+print(sm)
