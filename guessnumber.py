bb3b921279839135a71a82b0a660e1b3e7a53614
ra11 <rajeswarivinayagam05@gmail.com>
2024-01-15 19:10:08 +0400

    Update guessnumber.py

--- guessnumber.py
+++ guessnumber.py
@@ -6,11 +6,17 @@ Created on Tue Aug  8 11:08:55 2023
 """
 
 print("I have number from 1-20")
+# Initializing the number
 n=12
+
+#Taking Inputs
 guess = int(input("Enter any number: "))
+
+# Initializing the number of guesses.
 count=3
+# guesses depends upon range
 for i in range(count):
-    
+   # Condition testing 
     if guess < n:
         print("Too low")
         guess = int(input("Enter number again: "))
@@ -19,7 +25,8 @@ for i in range(count):
         guess = int(input("Enter number again: "))
     elif guess == n:
             print("Your correct!")
-                
+# If Guessing is more than required guesses,
+# shows this output.               
 print("You ran outo of attempts.The number was 12. Better Luck Next time!")
      
    
